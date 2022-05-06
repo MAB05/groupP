7605874f496398ff148f74065a31e46dd931e962
Dablnoy <go.8761@mail.ru>
2022-05-06 11:15:20 +0600

    Main commit

---
+++ src/components/Mycomponent-nonalcoholic.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+
+export default class MyComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          error: null,
+          isLoaded: false,
+          items: []
+        };
+    }
+   
+
+    
+    componentDidMount() {
+        fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic")
+          .then(res => res.json())
+          .then(
+            (result) => {
+              this.setState({
+                isLoaded: true,
+                items: result.drinks
+              });
+            },
+            (error) => {
+                this.setState({
+                  isLoaded: true,
+                  error
+                });
+              }
+            )
+        }
+    render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Ошибка: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Загрузка...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+                {item.strDrink}
+                <img width="50" height="50"  src={item.strDrinkThumb} alt="s"/>
+            </li>
+        
+          ))}
+        </ul>
+      );
+    }
+  }
+}
